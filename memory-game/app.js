6b09035badc18f70400d50b24766af3342f29b41
nkoubamarius <nkoubamarius@gmail.com>
2020-11-18 23:05:19 +0100

    Add functionality Flip your Card and the fonctionality check for matches

--- memory-game/app.js
+++ memory-game/app.js
@@ -6,12 +6,27 @@ document.addEventListener('DOMContentLoaded',()=>{
             img: 'images/fries.png',
         },
         {
+            name:'fries',
+            img: 'images/fries.png',
+        },
+        {
+            name:'cheeseburger',
+            img: 'images/cheeseburger.png',
+        },
+        {
             name:'cheeseburger',
             img: 'images/cheeseburger.png',
         },
         {
             name:'hotdog',
             img: 'images/hotdog.png',
+        },{
+            name:'hotdog',
+            img: 'images/hotdog.png',
+        },
+        {
+            name:'ice-cream',
+            img: 'images/ice-cream.png',
         },
         {
             name:'ice-cream',
@@ -22,36 +37,26 @@ document.addEventListener('DOMContentLoaded',()=>{
             img: 'images/milkshake.png',
         },
         {
+            name:'milkshake',
+            img: 'images/milkshake.png',
+        },
+        {
             name:'pizza',
             img: 'images/pizza.png',
         },
         {
-            name:'white',
-            img: 'images/white.png',
-        },
-        {
-            name:'white',
-            img: 'images/white.png',
-        },
-        {
-            name:'white',
-            img: 'images/white.png',
-        },
-        {
-            name:'white',
-            img: 'images/white.png',
-        },
-        {
-            name:'white',
-            img: 'images/white.png',
-        },
-        {
-            name:'white',
-            img: 'images/white.png',
+            name:'pizza',
+            img: 'images/pizza.png',
         }
     ];
 
+    cardArray.sort(()=> 0.5-Math.random())
+
     const grid=document.querySelector('.grid');
+    const resultDisplay=document.querySelector('#result')
+    var cardsChosen=[];
+    var cardsChosenId=[];
+    var cardsWon=[];
 
     //create your board
     function createBoard(){
@@ -59,11 +64,46 @@ document.addEventListener('DOMContentLoaded',()=>{
             var card =document.createElement('img');
             card.setAttribute('src', 'images/blank.png')
             card.setAttribute('data-id', i)
-            //card.addEventListener('click', flipcard)
+            card.addEventListener('click', flipCard)
 
             grid.appendChild(card)
         }
     }
+
+    //check for macthes
+    function checkForMatch(){
+        var cards=document.querySelectorAll('img');
+        const optionOneId=cardsChosenId[0]
+        const optionTwoId=cardsChosenId[1]
+        if(cardsChosen[0]===cardsChosen[1]){
+            alert('you found a match');
+            cards[optionOneId].setAttribute('src','images/white.png');
+            cards[optionTwoId].setAttribute('src', 'images/white.png');
+            cardsWon.push(cardsChosen)
+        }else{
+            cards[optionOneId].setAttribute('src','images/blank.png');
+            cards[optionTwoId].setAttribute('src', 'images/blank.png');
+            alert('Try again');
+        }
+        cardsChosen=[]
+        cardsChosenId=[]
+        resultDisplay.textContent=cardsWon.length
+        if(cardsWon.length===cardArray.length/2){
+            resultDisplay.textContent='congratulations! you found them all'
+        }
+    }
+
+    //flip your card
+    function flipCard(){
+        var cardId=this.getAttribute('data-id');
+        cardsChosen.push(cardArray[cardId].name);
+        cardsChosenId.push(cardId);
+        this.setAttribute('src',cardArray[cardId].img);
+        if(cardsChosen.length===2){
+            setTimeout(checkForMatch, 500)
+        }
+    }
+
 
     createBoard();
 
